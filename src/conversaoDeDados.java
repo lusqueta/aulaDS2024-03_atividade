74ac5670231f3d9d8c60d06ba45adc9a3778794b
LUCAS_3941 <lucas.araujo.pereira23@gmail.com>
2024-03-19 13:27:15 -0300

    Initial commit

---
+++ src/conversaoDeDados.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class conversaoDeDados {
+    public static void main(String[] args) {
+        int v1 = 23;
+
+        float valor2 = Float.valueOf(v1);
+
+        System.out.println("O valor transferido é: "+ valor2);
+    }
+}
---
+++ src/mensagem.java
@@ -0,0 +1,5 @@
+public class mensagem {
+    public static void main(String[] args) {
+        System.out.println("Essa é uma mensagem massa na tela");
+    }
+}
